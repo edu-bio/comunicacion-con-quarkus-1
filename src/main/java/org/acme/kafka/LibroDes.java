b95ecc919a743cda07c4ab020b89c8f1c676651b
Edu <61661507+edu-bio@users.noreply.github.com>
2021-02-12 14:31:23 +0100

    probando cositas

---
+++ src/main/java/org/acme/kafka/LibroDes.java
@@ -0,0 +1,11 @@
+package org.acme.kafka;
+
+import io.quarkus.kafka.client.serialization.JsonbDeserializer;
+
+public class LibroDes extends JsonbDeserializer<Libro> {
+
+	public LibroDes() {
+		super(Libro.class);
+		// TODO Auto-generated constructor stub
+	}
+}
